8e73bce7c5e3e6454ad6e0405bb24af8535e747a
Unknown <waynene0831@hotmail.com>
2018-03-05 09:17:46 +0800

    Merge branch 'develop'

---
+++ 拔出來的function.js
@@ -0,0 +1,322 @@
+createBody(){
+			this.$ele.append('<div class="calendars calendars_daymode"></div>');        
+            //月份
+            this.$ele.find('.calendars').append('<div class="calendars_tabWrap"><a class="pre"></a><ul class="newtab"></ul><a class="next"></div>')
+            //星期
+            this.$ele.find('.calendars').append('<div class="calendars_weeksWrap"><span>星期日</span><span>星期一</span><span>星期二</span><span>星期三</span><span>星期四</span><span>星期五</span><span>星期六</span></div>');
+           	//日期
+            this.$ele.find('.calendars').append('<ul class="calendars_daysWrap"></ul>')
+
+	return this
+}
+
+
+
+createCalendar(){
+	const anotherfirstday = aaayear+'/'+aaamonth+'/'+'01';
+		    const adate =  new Date(anotherfirstday);// 目前是用預設月份產月曆         
+	        const mYear =adate.getFullYear();       //本年 //const mYear = date.getFullYear(); 
+	        const mMonth = adate.getMonth();
+	        const day_list = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+			//console.log(day_list[weekday]);
+	        let limMonth =mMonth+1;//本月
+	        //const mMonth = date.getMonth(); 
+	        const startDay = adate.getDay();//補滿最後一排月曆開頭的幾天
+            const mDays = new Array(31,28,31,30,31,30,31,31,30,31,30,31);
+            if ( ( (mYear % 4 == 0) && (mYear % 100 != 0) ) || (mYear % 400 == 0) ) mDays[1] = 29;
+            const nDays = mDays[mMonth];		
+		    const total = mDays[mMonth] + startDay;
+            // console.log(nDays);//當月天數
+		    const totalCells = total + ( total%7 ? 7 - total%7 : 0 );
+		    const lastDay = totalCells - nDays - startDay;
+		    // console.log(lastDay);//補滿最後一排月曆剩下的幾天
+	        var i;        //日期
+	        var html = '';
+	        html += '';
+	        for (i = 0; i < startDay; i++) {
+	            html += '<li class="lastmonth"><div></div></li>';
+	        }	
+	        //console.log(daydateControl);
+	        for (var j = 1; j <= nDays; j++) {//mYear mMonth j
+	        		let weekday;
+	        		let daynum;
+	        		let monthnum;
+		        	if(j<10){ daynum = '0'+j;}else{daynum = j;}
+			        if(limMonth<10){ monthnum='0'+limMonth }else{monthnum=limMonth}
+	                html += '<li class="days'+" "+mYear+monthnum+daynum+'"><div class="date"><span class="num">';
+	                const cuttectDate = ''.concat(mYear, monthnum, daynum);
+					const weekdate =  new Date(mYear+'/'+monthnum+'/'+daynum);   
+	        		weekday = weekdate.getDay();
+	                 // console.log(cuttectDate);
+	                html += j+'</span><span class="weekday">'+day_list[weekday]+'</span></div>'; //加日期
+
+	                //---------對日期塞detail 
+	         		for(let k = 0; k<numofdata; k++){
+				         const DD = parseInt(daydateControl[k].date.substring(8.9));//全部的天數//
+				         const MM = parseInt(daydateControl[k].date.substring(5.7));//月份
+				         let ddd; 
+				         let mmm;
+				         if (DD<10){ddd="0"+DD;}else{ddd=DD;}	        
+				         if (MM<10){mmm="0"+MM;}else{mmm=MM;}
+
+				         const YYYY = parseInt(daydateControl[k].date.substring(0.4));//年份
+				         const rr =	''.concat(YYYY,mmm, ddd);
+				         //console.log(rr);     
+				        if(cuttectDate===rr){      	
+				       		if(daydateControl[k].status==='額滿' ||daydateControl[k].status==='截止' ||daydateControl[k].status==='後補'){
+						       html += '<span class="status lg-org">'+daydateControl[k].status+'</span>' ;
+						     }
+						     else{
+						       html += '<span class="status">'+daydateControl[k].status+'</span>' ;
+						     };
+						    html +='<span class="sell">可賣 :  '+daydateControl[k].availableVancancy+'</span><span class="group">團位 : '+daydateControl[k].totalVacnacy+'</span><span class="price">$'+daydateControl[k].price+'</span>'
+				       		if(daydateControl[k].guaranteed == true){
+				       		html +='<span class="tip">保證出團<span>';
+				       		}
+				        }		       		
+				    	};	
+	                //---------對日期塞detail end
+	                html += '</div></li>';}           
+
+              //--------------------------------------------
+		        for (i = 0; i<lastDay; i++) {
+		            html += '<li class="nextmonth"></li>';
+		        }
+	         //--------------------------------------------
+		         if(totalCells<=35){
+		        	for (i = 0; i<7; i++) {
+		          html += '<li class="nextmonth"></li>'
+		    	}
+	         };
+	         html +='<div class="listPage"><span class="listChangeBox"><a class="prevList"><span class="arrow-gl m-r-xs"></span>上一頁</a></span><span class="num"><span class="current_page"></span><span style="padding:0 3px;">/</span><span class="total_page"></span></span><span class="listChangeBox"><a class="nextList">下一頁<span class="arrow-gr m-lr-xs"></span></a></span></div>';        
+	return this
+}
+
+getajax(){
+			self=this;
+            $.ajax({
+			method: 'GET',
+			url: './json/data3.json',
+			dataType: 'json'
+			}).done(function(data) {
+             let daydata;
+             daydata = data;
+
+
+             //---------------------------篩選json資料重複的------------------------
+			  var lookup = {};
+			  var items = daydata;
+			  daydata = [];
+
+			  for (var item, i = 0; item = items[i++];) {
+
+			  var date = item.date ;
+
+			  //不同資料的key 刪除再新增
+			  
+			  var statusKey = (item.status||item.state) ;
+              delete(item.status||item.state) ;
+              item.status = statusKey ;	
+              
+			  var availableVancancyKey = (item.availableVancancy||item.onsell) ;
+              delete(item.availableVancancy||item.onsell) ;
+              item.availableVancancy = availableVancancyKey ;	
+
+              var totalVacnacyKey = (item.totalVacnacy||item.total) ;
+              delete(item.totalVacnacy||item.total) ;
+              item.totalVacnacy = totalVacnacyKey ;
+
+              var guaranteedKey = (item.guaranteed || item.certain) ;
+              delete(item.guaranteed || item.certain) ;
+              item.guaranteed = guaranteedKey ;
+
+			  if (!(date in lookup)) {
+			    lookup[date] = 1;
+			    daydata.push(item);
+			  }
+			}
+			 //---------------------------篩選json資料重複的 end------------------------
+
+            const daydateControl = daydata.sort(function (a, b) {
+				 return a.date > b.date ? 1 : -1;
+				});//用date排序
+            //console.log(daydateControl);//排列過的順序
+      		const numofdata = daydateControl.length;
+
+      		});
+	return this
+}
+
+inputajax(inputOpt){
+$.ajax({
+                dataType: "json",
+                method: 'GET',
+                url: './json/data4.json'
+            }).done(function(data){
+
+            		let daydata  = data;
+					console.log('111');
+				 	console.log(daydata);//進入的數量
+
+            		daydata = daydata.concat(inputOpt);   
+					console.log('222');
+			 	 	console.log(daydata);//input加進來後的數量
+
+                	//---------------------------篩選json資料重複的------------------------
+					 var lookup = {};
+					 var items = daydata;
+				     daydata = [];
+
+					 for (var item, i = 0; item = items[i++];) {
+
+					  var date = item.date ;
+
+					  //不同資料的key 刪除再新增
+					  var statusKey = (item.status||item.state) ;
+		              delete(item.status||item.state) ;
+		              item.status = statusKey ;	
+		              
+
+					  var availableVancancyKey = (item.availableVancancy||item.onsell) ;
+		              delete(item.availableVancancy||item.onsell) ;
+		              item.availableVancancy = availableVancancyKey ;	
+
+
+		              var totalVacnacyKey = (item.totalVacnacy||item.total) ;
+		              delete(item.totalVacnacy||item.total) ;
+		              item.totalVacnacy = totalVacnacyKey ;
+
+
+		              var guaranteedKey = (item.guaranteed || item.certain) ;
+		              delete(item.guaranteed || item.certain) ;
+		              item.guaranteed = guaranteedKey ;
+
+
+					  if (!(date in lookup)) {
+					    lookup[date] = 1;
+					    daydata.push(item);
+					  }
+					}	
+					//---------------------------篩選json資料重複的------------------------
+
+	             daydata = daydata.sort(function (a, b) {
+					 return a.date > b.date ? 1 : -1;
+					});//用date排序
+	             	console.log('333');
+			 	 	console.log(daydata);//篩選掉重複後的資料數量
+            });
+
+	return this
+}
+
+createCalendar(inputdata){
+    				//----------------------------排列過的順序
+
+    	      		let creatnYear = parseInt($('.newtab').find('.now').text().substring(0,4));
+                	let creatnMonth = parseInt($('.newtab').find('.now').text().substring(5,7))-1;       
+            	    //console.log(creatnYear);
+                	//console.log(creatnMonth);
+                	let creatnewTime = moment().set({'year': creatnYear, 'month': creatnMonth});
+                    //console.log(creatnewTime);
+                    const newdate =  new Date(creatnewTime);
+                    //console.log(newdate);
+                    const mYear =newdate.getFullYear();       
+                    //本年 //const mYear = date.getFullYear(); 
+			        const mMonth = newdate.getMonth();   
+			        const limMonth = mMonth+1;    //本月 
+
+					const day_list = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+
+			        //const mMonth = date.getMonth(); 
+			       	newdate.setDate(1);
+			        const startDay = newdate.getDay();//4
+		            const mDays = new Array(31,28,31,30,31,30,31,31,30,31,30,31);
+		            if ( ( (mYear % 4 == 0) && (mYear % 100 != 0) ) || (mYear % 400 == 0) ) mDays[1] = 29;
+		            const nDays = mDays[mMonth];		
+				    const total = mDays[mMonth] + startDay;
+		            // console.log(nDays);//當月天數	    		            
+				    const totalCells = total + ( total%7 ? 7 - total%7 : 0 );
+				    const lastDay = totalCells - nDays - startDay;
+				    var numRow = 0;  //  每7個td  +tr
+			        var i;        //日期
+			        let newlist;
+					newlist ='';
+			        var html = '';
+			        for (i = 0; i < startDay; i++) {
+			            html += '<li class="lastmonth"><div></div></li>';
+			        }
+			        //--------------------------------------------
+			        for (var j = 1; j <= nDays; j++) {
+			        	let weekday;
+			        	let daynum;
+		        		let monthnum;
+			        	if(j<10){ daynum = '0'+j;}else{daynum = j;}
+				        if(limMonth<10){ monthnum='0'+limMonth }else{ monthnum=limMonth }
+			                html += '<li class="days'+" "+mYear+monthnum+daynum+'"><div class="date"><span class="num">';
+			            	const cuttectDate = ''.concat(mYear, monthnum, daynum);
+			            	const weekdate =  new Date(mYear+'/'+monthnum+'/'+daynum);
+	        				weekday = weekdate.getDay();
+			                 //加日期
+			                html += j+'</span><span class="weekday">'+day_list[weekday]+'</span></div>';
+			                //---------對日期塞detail 
+			         		for(let k = 0; k<numofdata; k++){ 
+						         const DD = parseInt(inputdata[k].date.substring(8.9));//全部的天數//				         
+						         const MM = parseInt(inputdata[k].date.substring(5.7));//月份
+						
+						         let ddd; if (DD<10){ddd="0"+DD;}else{ddd=DD;}	
+						         let mmm; if (MM<10){mmm="0"+MM;}else{mmm=MM;}						                 						         
+						         const YYYY = parseInt(inputdata[k].date.substring(0.4));//年份
+						         const rr =	''.concat(YYYY,mmm, ddd);
+
+						       if(cuttectDate===rr){      	
+					       		if(inputdata[k].status==='額滿' ||inputdata[k].status==='截止' ||inputdata[k].status==='後補'){
+							       html += '<span class="status lg-org">'+inputdata[k].status+'</span>' ;
+							       }
+							    else{
+							       html += '<span class="status">'+inputdata[k].status+'</span>' ;
+							        	 };
+							       html +='<span class="sell">可賣 : '+inputdata[k].availableVancancy+'</span><span class="group">團位 : '+inputdata[k].totalVacnacy+'</span><span class="price">$'+inputdata[k].price+'</span>'
+					       		if(inputdata[k].guaranteed == true){
+					       		   html +='<span class="tip">保證出團<span>';
+					       		}
+					        	}		       		
+						    	};
+			                //---------對日期塞detail end
+			                html += '</div></li>';
+			                }
+
+		            //--------------------------------------------
+			        for (i = 0; i<lastDay; i++) {
+			            html += '<li class="nextmonth"></li>';
+			        }
+			        //--------------------------------------------
+			        if(totalCells<=35){
+				        	for (i = 0; i<7; i++) {
+				          html += '<li class="nextmonth"></li>'
+				    	}
+			         };
+			    html +='<div class="listPage"><span class="listChangeBox"><a class="prevList"><span class="arrow-gl m-r-xs"></span>上一頁</a></span><span class="num"><span class="current_page"></span><span style="padding:0 3px;">/</span><span class="total_page"></span></span><span class="listChangeBox"><a class="nextList">下一頁<span class="arrow-gr m-lr-xs"></span></a></span></div>';
+	        
+			    $('.calendars_daysWrap').append(html);	       
+
+			    $('.calendars_daysWrap').find('.price').parent().addClass('hasdata'); 
+
+			    $('.calendars').find('.hasdata').on('click', function() { 
+                $('.calendars').find('.hasdata').removeClass('active');
+                $(this).addClass('active');
+            	});
+
+			    self.listpage();
+	return this
+}
+
+let initYearMonth = self.option.initYearMonth ;
+				//console.log(initYearMonth);
+			let tabhtml = '';
+			for ( i=0 ; i<4 ; i++ ) {
+			let nextMonth = moment(initYearMonth).add(i, 'months').format("YYYY MM");
+				//console.log(nextMonth);
+			tabhtml += '<li class="tab"><a href="#"><span>'+ nextMonth +'月</span></a></li>'
+				}
+				//console.log(tabhtml);
+			$('.newtab').append(tabhtml);//產生initYearmonth設定的月
